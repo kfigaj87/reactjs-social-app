13e5175ec0fa301d04de583c8e888d4dca85702d
kfigaj <kfigaj87@gmail.com>
2022-10-25 21:21:51 +0200

    update 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import Nav from "./components/Nav";
-import Post from "./components/Post";
+// import Post from "./components/Post";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState();
+  axios.defaults.headers.common["Authorization"] =
+    "Bearer " + (user ? user.jwt_token : "");
+  axios.defaults.headers.post["Content-Type"] = "application/json";
+
+  console.log(setUser);
+
+  // const handleInputChange = (e) => {
+  //   const target = e.target;
+  //   const name = target.name;
+  //   setFromData({
+  //     ...formData,
+  //     [name]: target.value,
+  //   });
+
   return (
     <div className="App">
       <Nav />
@@ -12,5 +28,6 @@ function App() {
     </div>
   );
 }
+// }
 
 export default App;
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Login.css";
+import axios from "axios";
+import { useState } from "react";
 
-const Login = () => {
+const Login = (props) => {
+  const [loginMessage, setLoginMessage] = useState();
+
+  const handleSubmit = () => {
+    console.log("działam");
+    axios
+      .post("http://akademia108.pl/api/social-app/user/login")
+      .then((res) => {
+        const loginRes = res.data;
+        setLoginMessage(loginRes);
+      });
+  };
+
+  useEffect(() => {
+    handleSubmit();
+  }, []);
+
   return (
     <div className="login">
       <h2 className="nav-h">Login</h2>
+      <div className="form-log">
+        <form>
+          <label htmlFor="username">Username </label>
+          <input type="text" name="username" placeholder="username" />
+          <label htmlFor="password"> Password </label>
+          <input type="password" name="password" placeholder="password" />
+          <button className="button btn-home" onClick={""}>
+            Sign up
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
